63c3d1b9d7f46d53b818c79702b20a91a626b33a
Konstantin Malanchev <hombit@gmail.com>
2024-06-19 10:39:34 -0400

    Optix optimizer

--- src/healpix_geometry_analysis/problems/tile.py
+++ src/healpix_geometry_analysis/problems/tile.py
@@ -1,8 +1,11 @@
 import dataclasses
 from typing import Literal, Self
 
+import jax
+import jax.numpy as jnp
 import numpyro
 import numpyro.distributions as dist
+import optax
 
 from healpix_geometry_analysis.coordinates import HealpixCoordinates
 
@@ -94,40 +97,37 @@ class TileProblem:
             distance=distance,
         )
 
-    def side1(self):
-        """Get k and kp indices for the first side of the tile
+    def numpyro_side1(self):
+        """Get k & k' numpyro samples for the first side of the tile
 
         It is NE for "p" direction and NW for "m" direction
         """
-        if self.direction == "p":
-            k = numpyro.sample("k1", dist.Uniform(self.k_center - self.delta, self.k_center + self.delta))
-            kp = self.kp_center - self.delta
-        elif self.direction == "m":
-            k = self.k_center - self.delta
-            kp = numpyro.sample("kp1", dist.Uniform(self.kp_center - self.delta, self.kp_center + self.delta))
-        else:
-            raise ValueError(f"Invalid direction: {self.direction}, must be one of {DIRECTIONS}")
-        return k, kp
+        return self._numpyro_side(1)
 
-    def side2(self):
-        """Get k and kp indices for the second side of the tile
+    def numpyro_side2(self):
+        """Get k & k' numpyro samples for the second side of the tile
 
         It is SW for "p" direction and SE for "m" direction
         """
+        return self._numpyro_side(2)
+
+    def _numpyro_side(self, index: Literal[1, 2]) -> tuple[object, object]:
+        k_name = f"k{index}"
+        kp_name = f"kp{index}"
         if self.direction == "p":
-            k = numpyro.sample("k2", dist.Uniform(self.k_center - self.delta, self.k_center + self.delta))
-            kp = self.kp_center + self.delta
+            k = numpyro.sample(k_name, self.free_parameter_distributions[k_name])
+            kp = self.frozen_parameters[kp_name]
         elif self.direction == "m":
-            k = self.k_center + self.delta
-            kp = numpyro.sample("kp2", dist.Uniform(self.kp_center - self.delta, self.kp_center + self.delta))
+            k = self.frozen_parameters[k_name]
+            kp = numpyro.sample(kp_name, self.free_parameter_distributions[kp_name])
         else:
             raise ValueError(f"Invalid direction: {self.direction}, must be one of {DIRECTIONS}")
         return k, kp
 
-    def model(self):
-        """Numpyro model for the tile distance problem"""
-        k1, kp1 = self.side1()
-        k2, kp2 = self.side2()
+    def numpyro_model(self):
+        """Numpyro model tp maximize"""
+        k1, kp1 = self.numpyro_side1()
+        k2, kp2 = self.numpyro_side2()
 
         if self.distance == "chord_squared":
             distance = self.coord.chord_squared(k1, kp1, k2, kp2)
@@ -141,3 +141,152 @@ class TileProblem:
             raise ValueError(f"Invalid distance: {self.distance}, must be one of {DISTANCE}")
 
         numpyro.deterministic("distance", distance)
+
+    parameter_names = ["k1", "k2", "kp1", "kp2"]
+
+    @property
+    def frozen_parameters(self) -> dict[str, object]:
+        """Frozen parameters for the problem
+
+        Returns
+        -------
+        tuple[str, str]
+            Freezed parameters.
+            ("k1" and "k2") for "m" direction
+            and ("kp1" and "kp2") for "p" direction
+        """
+        if self.direction == "p":
+            return {"kp1": self.kp_center - self.delta, "kp2": self.kp_center + self.delta}
+        if self.direction == "m":
+            return {"k1": self.k_center - self.delta, "k2": self.k_center + self.delta}
+        raise ValueError(f"Invalid direction: {self.direction}, must be one of {DIRECTIONS}")
+
+    @property
+    def free_parameter_limits(self) -> dict[str, tuple[object, object]]:
+        """Free parameters for the problem and their limits
+
+        Returns
+        -------
+        dict[str, tuple[object, object]]
+            Free parameters and their lower and upper limits.
+            ("k1" and "k2") for "m" direction
+            and ("kp1" and "kp2") for "p" direction
+        """
+        if self.direction == "p":
+            limits = (self.k_center - self.delta, self.k_center + self.delta)
+            return dict.fromkeys(["k1", "k2"], limits)
+        if self.direction == "m":
+            limits = (self.kp_center - self.delta, self.kp_center + self.delta)
+            return dict.fromkeys(["kp1", "kp2"], limits)
+        raise ValueError(f"Invalid direction: {self.direction}, must be one of {DIRECTIONS}")
+
+    @property
+    def free_parameter_distributions(self) -> dict[str, dist.Distribution]:
+        """Free parameters for the problem and their distributions
+
+        Returns
+        -------
+        dict[str, dist.Distribution]
+            Free parameters and their distributions.
+            ("k1" and "k2") for "m" direction
+            and ("kp1" and "kp2") for "p" direction
+        """
+        return {name: dist.Uniform(*limits) for name, limits in self.free_parameter_limits.items()}
+
+    def params(self, rng_key: jax.random.PRNGKey) -> dict[str, object]:
+        """Initial parameter values
+
+        Parameters
+        ----------
+        rng_key : jax.random.PRNGKey
+            Random number generator key
+
+        Returns
+        -------
+        dict[str, object]
+            Initial parameter values, free parameters are sampled from
+            the uniform distribution within their limits, and frozen
+            parameters are set to their values.
+        """
+        random_free_params = {}
+        for name, distribution in self.free_parameter_distributions.items():
+            random_free_params[name] = distribution.sample(rng_key)
+            rng_key = jax.random.split(rng_key)[0]
+        all_params = random_free_params | self.frozen_parameters
+        return {name: all_params[name] for name in self.parameter_names}
+
+    @property
+    def limits(self) -> dict[str, tuple[object, object]]:
+        """Limits for the parameters
+
+        Returns
+        -------
+        dict[str, tuple[lower, upper]]
+            Limits for the parameters. Frozen parameters have infinite limits.
+        """
+        frozen_parameters = dict.fromkeys(self.frozen_parameters, (-jnp.inf, jnp.inf))
+        all_parameters = frozen_parameters | self.free_parameter_limits
+        return {name: all_parameters[name] for name in self.parameter_names}
+
+    @property
+    def lower_bounds(self) -> dict[str, object]:
+        """Lower limits for the parameters
+
+        Returns
+        -------
+        dict[str, object]
+            Lower limits for the parameters. Frozen parameters have -inf limits.
+        """
+        return {name: limits[0] for name, limits in self.limits.items()}
+
+    @property
+    def upper_bounds(self) -> dict[str, object]:
+        """Upper limits for the parameters
+
+        Returns
+        -------
+        dict[str, object]
+            Upper limits for the parameters. Frozen parameters have inf limits.
+        """
+        return {name: limits[1] for name, limits in self.limits.items()}
+
+    def freeze_optax_optimizer(self, optimizer):
+        """Freeze parameters of the optimizer"""
+        transforms = {"optimizer": optimizer, "frozen": optax.set_to_zero()}
+        param_labels = {frozen: "frozen" for frozen in self.frozen_parameters} | {
+            free: "optimizer" for free in self.free_parameter_limits
+        }
+        return optax.multi_transform(transforms, param_labels)
+
+    def optix_side1(self, params):
+        """Get k & k' from the optimizer parameters for the first side of the tile"""
+        return self._optix_side(params, 1)
+
+    def optix_side2(self, params):
+        """Get k & k' from the optimizer parameters for the second side of the tile"""
+        return self._optix_side(params, 2)
+
+    def _optix_side(self, params, index: Literal[1, 2]) -> tuple[object, object]:
+        k_name = f"k{index}"
+        kp_name = f"kp{index}"
+        if self.direction == "p":
+            k = params[k_name]
+            kp = self.frozen_parameters[kp_name]
+        elif self.direction == "m":
+            k = self.frozen_parameters[k_name]
+            kp = params[kp_name]
+        else:
+            raise ValueError(f"Invalid direction: {self.direction}, must be one of {DIRECTIONS}")
+        return k, kp
+
+    def optix_loss(self, params):
+        """Loss function to minimize with optix"""
+        k1, kp1 = self.optix_side1(params)
+        k2, kp2 = self.optix_side2(params)
+
+        if self.distance == "chord_squared":
+            return self.coord.chord_squared(k1, kp1, k2, kp2)
+        if self.distance == "cos_arc":
+            # Use negative cosine to maximize it and minimize distance
+            return -self.coord.cos_arc(k1, kp1, k2, kp2)
+        raise ValueError(f"Invalid distance: {self.distance}, must be one of {DISTANCE}")
--- tests/healpix_geometry_analysis/problems/test_tile.py
+++ tests/healpix_geometry_analysis/problems/test_tile.py
@@ -1,23 +1,21 @@
 import jax
 import jax.numpy as jnp
+import optax
 from healpix_geometry_analysis.problems.tile import TileProblem
 from numpyro.infer import MCMC, NUTS
 
 
 def test_tile_problem_nuts():
-    """e2e test for TileProblem"""
+    """e2e test for TileProblem with MCMC sampler"""
     problem = TileProblem.from_order(
         order=4,
-        # k_center=1.5,
-        # kp_center=14.5,
-        k_center=10.5,
-        kp_center=15.5,
-        direction="m",
-        # distance="cos_arc",
+        k_center=1.5,
+        kp_center=14.5,
+        direction="p",
         distance="chord_squared",
     )
 
-    kernel = NUTS(problem.model)
+    kernel = NUTS(problem.numpyro_model)
     mcmc = MCMC(kernel, num_warmup=0, num_samples=100)
     rng_key = jax.random.PRNGKey(0)
     mcmc.run(rng_key)
@@ -29,23 +27,29 @@ def test_tile_problem_nuts():
     argmin = jnp.argmin(arc_distance_deg)
     min_distance = arc_distance_deg[argmin]
     assert min_distance < problem.coord.grid.average_pixel_size_degree
-    # assert min_distance == 0, \
-    #     (
-    #         f"min_distance = {min_distance:.3f} deg, "
-    #         f"average_pixel_size = {problem.coord.grid.average_pixel_size_degree:.3f} deg, "
-    #         f"k'1 = {samples['kp1'][argmin]:.3f}, k'2 = {samples['kp2'][argmin]:.3f}"
-    #     )
 
-    # import matplotlib.pyplot as plt
-    #
-    # plt.hist2d(samples["k1"], samples["k2"], bins=15)
-    # plt.colorbar()
-    # plt.xlabel("k1")
-    # plt.ylabel("k2")
-    # plt.show()
-    # plt.close()
-    #
-    # plt.hist(arc_distance_deg, bins=100)
-    # plt.xlabel("distance, deg")
-    # plt.show()
-    # plt.close()
+
+def test_tile_problem_adam():
+    """ "e2e test for TileProblem with Adam optimizer"""
+    problem = TileProblem.from_order(
+        order=7,
+        k_center=(1 << 7) - 10.5,
+        kp_center=7.5,
+        direction="m",
+        distance="chord_squared",
+    )
+
+    optimizer = problem.freeze_optax_optimizer(optax.adabelief(1e-1))
+    rng_key = jax.random.PRNGKey(0)
+    params = problem.params(rng_key)
+    opt_state = optimizer.init(params)
+    value_and_grad = jax.jit(jax.value_and_grad(problem.optix_loss))
+
+    for _ in range(100):
+        loss, grads = value_and_grad(params)
+        updates, opt_state = optimizer.update(grads, opt_state, params)
+        params = optax.apply_updates(params, updates)
+        params = optax.projections.projection_box(params, problem.lower_bounds, problem.upper_bounds)
+
+    arc_distance_deg = jnp.degrees(2.0 * jnp.arcsin(0.5 * jnp.sqrt(loss)))
+    assert arc_distance_deg < problem.coord.grid.average_pixel_size_degree
